1e340916f3b4bc17698e3a8a91466c5fa7508a02
EC2 Default User <ec2-user@ip-172-31-23-23.ap-southeast-1.compute.internal>
2020-04-07 19:04:31 +0000

    tutorial_two_end

--- main.rb
+++ main.rb
@@ -1,49 +1,26 @@
 class Brave
-    def initialize
-        @name = "テリー"
-        @hp = 500
-        @offense = 150
-        @defense = 100
-    end
+    attr_reader :name, :offense, :defense
+    attr_accessor :hp
     
-    def name=(name)
-        @name = name
-    end
     
-    def name
-        @name
-    end
-    
-    def hp=(hp)
-        @hp = hp
-    end
-    
-    def hp
-        @hp
-    end
-    
-    def offense=(offense)
-        @offense = offense
-    end
-    
-    def offense
-        @offense
-    end
-    
-    def defense=(defense)
-        @defense = defense
-    end
-    
-    def defense
-        @defense
+    def initialize(**params)
+        @name = params[:name]
+        @hp = params[:hp]
+        @offense = params[:offense]
+        @defense = params[:defense]
     end
 
 end
 
-brave = Brave.new
+brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 
 
 puts "NAME:#{brave.name}"
 puts "HP:#{brave.hp}"
 puts "OFFENSE:#{brave.offense}"
 puts "DEFENSE:#{brave.defense}"
+
+
+brave.hp -= 30
+
+puts "#{brave.name}はダメージを受けた！　残りHPは#{brave.hp}だ"
