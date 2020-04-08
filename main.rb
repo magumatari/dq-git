dd141f1664a8c0c3e5c8e0980db62eafe1239a56
EC2 Default User <ec2-user@ip-172-31-23-23.ap-southeast-1.compute.internal>
2020-04-08 11:33:57 +0000

    doragon_in

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ class Brave
     attr_reader :name, :offense, :defense
     attr_accessor :hp
     
+    SPECIAL_ATTACK_CONSTANT = 1.5
     
     def initialize(**params)
         @name = params[:name]
@@ -9,18 +10,59 @@ class Brave
         @offense = params[:offense]
         @defense = params[:defense]
     end
+    
+    def attack(monster)
+        
+        puts "#{@name}の攻撃"
+        
+        attack_num = rand(4)
+        
+        if attack_num == 0
+            puts "必殺攻撃"
+            damage = calculate_special_attack - monster.defense
+        else
+            puts "通常攻撃"
+            damage = @offense - monster.defense
+        end
+        
+        monster.hp -= damage
+        
+        puts "#{monster.name}は#{damage}のダメージを受けた"
+        puts "#{monster.name}の残りHPは#{monster.hp}だ"
+    end
+    
+    def calculate_special_attack
+        @offense * SPECIAL_ATTACK_CONSTANT
+    end
 
 end
 
+class Monster
+    attr_reader :offense, :defense
+    attr_accessor :name, :hp
+    
+    def initialize(**params)
+        @name = params[:name]
+        @hp = params[:hp]
+        @offense = params[:offense]
+        @defense = params[:defense]
+    end
+    
+    def attack(brave)
+        puts "#{@name}の攻撃"
+        
+        damage = @offense - brave.defense
+        brave.hp -= damage
+        
+        puts "#{brave.name}は#{damage}のダメージを受けた"
+        puts "#{brave.name}の残りHPは#{brave.hp}だ"
+    end
+    
+end
+
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 
-puts "NAME:#{brave.name}"
-puts "HP:#{brave.hp}"
-puts "OFFENSE:#{brave.offense}"
-puts "DEFENSE:#{brave.defense}"
-
-
-brave.hp -= 30
-
-puts "#{brave.name}はダメージを受けた！　残りHPは#{brave.hp}だ"
+brave.attack(monster)
+monster.attack(brave)
