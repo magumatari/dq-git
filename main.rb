c3dae11c15a5db73eebbaabc2f88b71cfd940c3f
EC2 Default User <ec2-user@ip-172-31-23-23.ap-southeast-1.compute.internal>
2020-04-09 15:45:45 +0000

    file_split

--- main.rb
+++ main.rb
@@ -1,8 +1,6 @@
-class Brave
-    attr_reader :name, :offense, :defense
-    attr_accessor :hp
-    
-    SPECIAL_ATTACK_CONSTANT = 1.5
+class Character
+    attr_reader :offense, :defense
+    attr_accessor :name, :hp
     
     def initialize(**params)
         @name = params[:name]
@@ -10,6 +8,12 @@ class Brave
         @offense = params[:offense]
         @defense = params[:defense]
     end
+end
+        
+class Brave < Character
+    
+    SPECIAL_ATTACK_CONSTANT = 1.5
+
     
     def attack(monster)
         
@@ -66,10 +70,8 @@ class Brave
 
 end
 
-class Monster
-    attr_reader :offense, :defense
-    attr_accessor :name, :hp
-    
+class Monster < Character
+
     POWER_UP_RATE = 1.5
     CALC_HALF_HP = 0.5
     
@@ -145,4 +147,16 @@ loop do
     
     break if brave.hp <= 0
     
+end
+
+battle_result = brave.hp >0
+
+if battle_result
+    exp = (monster.offense + monster.defense) * 2
+    gold = (monster.offense + monster.defense) * 3
+    puts "#{brave.name}はたたかいに勝った"
+    puts "#{exp}の経験値と#{gold}ゴールドを獲得した"
+else
+    puts "#{brave.name}はたたかいに負けた"
+    puts "目の前が真っ暗になった" 
 end
