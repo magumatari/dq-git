9ca5c58eab9156d05083f3c024133f35572c2a38
EC2 Default User <ec2-user@ip-172-31-23-23.ap-southeast-1.compute.internal>
2020-04-09 14:30:00 +0000

    tutrial_tree_end

--- main.rb
+++ main.rb
@@ -54,6 +54,9 @@ class Brave
             target = params[:target]
             
             target.hp -= damage
+            
+            target.hp = 0 if target.hp < 0
+            
             puts "#{target.name}は#{damage}のダメージを受けた"
         end
         
@@ -111,6 +114,9 @@ class Monster
             target = params[:target]
                 
             target.hp -= damage
+            
+            target.hp =0 if target.hp < 0
+            
             puts "#{target.name}は#{damage}のダメージを受けた"
         end
         def transform
@@ -130,6 +136,13 @@ end
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
-
-brave.attack(monster)
-monster.attack(brave)
+loop do
+    brave.attack(monster)
+    
+    break if monster.hp <= 0
+    
+    monster.attack(brave)
+    
+    break if brave.hp <= 0
+    
+end
