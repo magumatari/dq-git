8df1a78946ac463ef1551c363e768954fb884f65
EC2 Default User <ec2-user@ip-172-31-23-23.ap-southeast-1.compute.internal>
2020-04-09 15:54:57 +0000

    4-4_start

---
+++ character.rb
@@ -0,0 +1,11 @@
+class Character
+    attr_reader :offense, :defense
+    attr_accessor :name, :hp
+    
+    def initialize(**params)
+        @name = params[:name]
+        @hp = params[:hp]
+        @offense = params[:offense]
+        @defense = params[:defense]
+    end
+end
